f1a142e5152240806856e59db7600ac554ae072c
soundarya5598 <31506045+soundarya5598@users.noreply.github.com>
2017-09-08 19:55:41 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("enter the character");
+scanf("%c",&ch);
+if(ch=='a'!!ch=='e'!!ch=='i'!!ch=='o'!!ch=='u'!!ch=='A'!!ch=='E'!!ch=='I'!!ch=='O'!!ch=='U')
+{
+printf("the character is vowel");
+}
+else
+{
+printf("the character is consonant");
+}
+return 0:
+}
