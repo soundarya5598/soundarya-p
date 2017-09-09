40493232f105c8ea6544e4ed053191a18528b3cd
soundarya5598 <31506045+soundarya5598@users.noreply.github.com>
2017-09-09 19:29:24 +0530

    Create Natural.c

---
+++ Natural.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n,sum=0;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("the sum of number%d",sum);
+}
+return 0;
+}
