12ccaadcf2d43a9b96a284a47142a58a35271a6b
soundarya5598 <31506045+soundarya5598@users.noreply.github.com>
2018-01-10 16:36:59 +0530

    Create 5multiples.c

---
+++ 5multiples.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,c,i;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+c=5*i;
+}
+printf("%dthe 5 multiples are",c);
+return 0;
+}
