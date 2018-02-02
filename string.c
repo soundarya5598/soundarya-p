9c967e9d0f6810e752e1d099a6ec12501c72de82
soundarya5598 <31506045+soundarya5598@users.noreply.github.com>
2018-02-02 16:46:07 +0530

    Update string.c

--- string.c
+++ string.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
 	int i,n;
 	char a[9];
 	scanf("%d",&n);
 	scanf("%s",&a);
-	for(i=0;i<=n;i++)
+	for(i=1;i<=n;i++)
 	{
 		printf("\n%s",a);
 	}
