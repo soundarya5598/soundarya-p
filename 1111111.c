628641289bbf00b2642dd25efc8ec88c91400e78
soundarya5598 <31506045+soundarya5598@users.noreply.github.com>
2018-02-05 17:00:41 +0530

    Create 1111111.c

---
+++ 1111111.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int i,a[20],avg,n,k=0;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+	printf("%d",&a[i]);
+}
+	}
+	for(i=1;i<=n;i++)
+	{
+		k=k+a[i];
+		avg=k/n;
+		printf("%d",avg);
+	}
+	return o;
+}
