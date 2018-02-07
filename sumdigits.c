186e36d6b6ee720202184f6baf81056e0f4078f5
soundarya5598 <31506045+soundarya5598@users.noreply.github.com>
2018-02-07 16:26:54 +0530

    Create sumdigits.c

---
+++ sumdigits.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+  int n,rem,b,sum=0;
+  scanf("%d",&n);
+  while(n>0)
+  {
+    rem=n%10;
+    n=n/10;
+    sum=sum+rem;
+  }
+  printf("%d",sum);
+  return 0;
+}
