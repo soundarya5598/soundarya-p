ec5282a0f7050f79654c7a0e35f1b689cac24313
soundarya5598 <31506045+soundarya5598@users.noreply.github.com>
2018-02-12 16:52:03 +0530

    Create mergesort.c

---
+++ mergesort.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+int quicksort(int num[25],int first,int last)
+{
+   int i, j, pivot, temp;
+
+   if(first<last){
+      pivot=first;
+      i=first;
+      j=last;
+
+      while(i<j){
+         while(num[i]<=num[pivot]&&i<last)
+            i++;
+         while(num[j]>num[pivot])
+            j--;
+         if(i<j){
+            temp=num[i];
+            num[i]=num[j];
+            num[j]=temp;
+         }
+      }
+
+      temp=num[pivot];
+      num[pivot]=num[j];
+      num[j]=temp;
+      quicksort(num,first,j-1);
+      quicksort(num,j+1,last);
+
+   }
+}
+
+int main(){
+   int i, count, num[25];
+
+   printf("enter the number of elements: ");
+   scanf("%d",&count);
+
+   printf("Enter %d elements: ", count);
+   for(i=0;i<count;i++)
+      scanf("%d",&num[i]);
+
+   quicksort(num,0,count-1);
+
+   printf("Order of Sorted elements: ");
+   for(i=0;i<count;i++)
+      printf(" %d",num[i]);
+      
+     
+int n=(sizeof(num)/sizeof(int));
+printf("\nthe median is %d",num[n]);
+
+
+   return 0;
+}
