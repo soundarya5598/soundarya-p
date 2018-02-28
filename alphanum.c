d06684fd3b0c60913d2f318daf1da1efede2df97
soundarya5598 <31506045+soundarya5598@users.noreply.github.com>
2018-02-28 17:00:12 +0530

    Create alphanum.c

---
+++ alphanum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+	char a[50];
+	int i;
+	scanf("%s",&a);
+	for(i=0;a[i]!='\0';i++)
+	{
+	if((a[i]>='0'&&a[i]<='9')||(a[i]>='a'&&a[i]<='z')||(a[i]>='A'&&a[i]<='Z'))
+	{
+	if(a[i]);
+	}
+	}
+	return 0;
+}
