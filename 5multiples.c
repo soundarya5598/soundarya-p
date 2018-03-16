998e33f6e8dd518d546a98cf9cace90cda948c84
soundarya5598 <31506045+soundarya5598@users.noreply.github.com>
2018-03-16 16:42:02 +0530

    Update 5multiples.c

--- 5multiples.c
+++ 5multiples.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
-#include<conio.h>
-void main()
+int main()
 {
 int n,c,i;
-printf("enter the number");
 scanf("%d",&n);
 for(i=1;i<=5;i++)
 {
-c=5*i;
+c=n*i;
+printf("%d ",c);
 }
-printf("%dthe 5 multiples are",c);
 return 0;
 }
