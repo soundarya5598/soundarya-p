924521f52dc56ccaedd805323cbf2f4da20dd279
soundarya5598 <31506045+soundarya5598@users.noreply.github.com>
2018-03-19 16:54:48 +0530

    Create strpalindrome.c

---
+++ strpalindrome.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char c[50],b[50];
+	gets(c);
+	b==c;
+	if(b==strrev(c))
+	{
+		printf("yes");
+	}
+else
+{
+	printf("no");
+}
+	
+	return 0;
+}
