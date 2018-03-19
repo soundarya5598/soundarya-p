ef08772f0b04dbbb879ed2fddb8f59d4726de57c
soundarya5598 <31506045+soundarya5598@users.noreply.github.com>
2018-03-19 15:41:57 +0530

    Create stringk.c

---
+++ stringk.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+   char a[50];
+   int n,k,i;
+   scanf("%s",&a[i]);
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+   	printf("%c",a[i]);
+   }
+	return 0;
+}
